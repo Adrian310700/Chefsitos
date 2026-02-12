7151ce93e53af24cd985bdb5bf1eeac94df8debb
Adrian310700 <andrik.mora.12@gmail.com>
2026-02-12 00:19:07 -0600

    Se implementaron los ultimos cuatro métodos para CarritoService

--- src/main/java/com/chefsitos/uamishop/ventas/service/CarritoService.java
+++ src/main/java/com/chefsitos/uamishop/ventas/service/CarritoService.java
@@ -1,31 +1,97 @@
 package com.chefsitos.uamishop.ventas.service;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
-import com.chefsitos.uamishop.ventas.domain.EstadoCarrito;
-import com.chefsitos.uamishop.ventas.domain.entity.ItemCarrito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.chefsitos.uamishop.ventas.domain.aggregate.Carrito;
 import com.chefsitos.uamishop.ventas.domain.valueObject.CarritoId;
 import com.chefsitos.uamishop.ventas.domain.valueObject.ClienteId;
-import com.chefsitos.uamishop.ventas.domain.valueObject.DescuentoAplicado;
-import com.chefsitos.uamishop.ventas.domain.valueObject.ItemCarritoId;
-import com.chefsitos.uamishop.ventas.domain.valueObject.ProductoRef;
-import com.chefsitos.uamishop.shared.domain.valueObject.Money;
-import com.chefsitos.uamishop.ventas.domain.aggregate.Carrito;
+import com.chefsitos.uamishop.ventas.repository.CarritoRepository;
 
 public class CarritoService {
+
+  @Autowired
+  CarritoRepository carritoRepository;
 
   public Carrito crear(ClienteId clienteId) {
     Carrito carrito = Carrito.crear(clienteId);
     return carrito;
   }
 
-  public obtenerCarrito(CarritoId carritoId){
+  public Carrito obtenerCarrito(CarritoId carritoId) {
 
+    Optional<Carrito> carrito = carritoRepository.findById(carritoId);
+
+    return carrito.orElseThrow(() -> new IllegalArgumentException("Carrito inexistente"));
 
   }
 
+  public Carrito vaciar(CarritoId carritoId) {
+
+    Carrito carrito = carritoRepository.findById(carritoId).orElse(null);
+    // Validamos que el carrito exista
+    if (carrito == null) {
+      throw new IllegalArgumentException("Carrito inexistente");
+    }
+
+    // Vaciamos el carrito
+    carrito.vaciar();
+
+    // Guardamos
+    carritoRepository.save(carrito);
+
+    return carrito;
+  }
+
+  public Carrito iniciarCheckout(CarritoId carritoId) {
+
+    Carrito carrito = carritoRepository.findById(carritoId).orElse(null);
+    // Validamos que el carrito exista
+    if (carrito == null) {
+      throw new IllegalArgumentException("Carrito inexistente");
+    }
+
+    // Iniciamos checkout
+    carrito.iniciarCheckout();
+
+    // Guardamo
+    carritoRepository.save(carrito);
+
+    return carrito;
+  }
+
+  public Carrito completarCheckout(CarritoId carritoId) {
+
+    Carrito carrito = carritoRepository.findById(carritoId).orElse(null);
+    // Validamos que el carrito exista
+    if (carrito == null) {
+      throw new IllegalArgumentException("Carrito inexistente");
+    }
+
+    // Completamos Checkout
+    carrito.completarCheckout();
+
+    // Guardamos
+    carritoRepository.save(carrito);
+
+    return carrito;
+  }
+
+  public Carrito abandonar(CarritoId carritoId) {
+
+    Carrito carrito = carritoRepository.findById(carritoId).orElse(null);
+    // Validamos que el carrito exista
+    if (carrito == null) {
+      throw new IllegalArgumentException("Carrito inexistente");
+    }
+
+    // Abandonamos
+    carrito.abandonar();
+
+    // Guardamos
+    carritoRepository.save(carrito);
+
+    return carrito;
+  }
 }
