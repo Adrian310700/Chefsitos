291aeed9a2546bf1af14457469e7d6bd2754847c
terPM <teresaguadalupepimentel@gmail.com>
2026-02-13 21:10:50 -0600

    Add metodos faltantes del servicio Carrito

--- src/main/java/com/chefsitos/uamishop/ventas/repository/CarritoRepository.java
+++ src/main/java/com/chefsitos/uamishop/ventas/repository/CarritoRepository.java
@@ -1,10 +1,18 @@
 package com.chefsitos.uamishop.ventas.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chefsitos.uamishop.ventas.domain.EstadoCarrito;
 import com.chefsitos.uamishop.ventas.domain.aggregate.Carrito;
 import com.chefsitos.uamishop.ventas.domain.valueObject.CarritoId;
+import com.chefsitos.uamishop.ventas.domain.valueObject.ClienteId;
 
+@Repository
 public interface CarritoRepository extends CrudRepository<Carrito, CarritoId> {
+
+    Optional<Carrito> findByClienteIdAndEstado(ClienteId clienteId, EstadoCarrito estado);
 
 }
--- src/main/java/com/chefsitos/uamishop/ventas/service/CarritoService.java
+++ src/main/java/com/chefsitos/uamishop/ventas/service/CarritoService.java
@@ -1,26 +1,46 @@
 package com.chefsitos.uamishop.ventas.service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.chefsitos.uamishop.shared.domain.valueObject.Money;
+import com.chefsitos.uamishop.ventas.domain.EstadoCarrito;
 import com.chefsitos.uamishop.ventas.domain.aggregate.Carrito;
 import com.chefsitos.uamishop.ventas.domain.valueObject.CarritoId;
 import com.chefsitos.uamishop.ventas.domain.valueObject.ClienteId;
+import com.chefsitos.uamishop.ventas.domain.valueObject.ProductoRef;
 import com.chefsitos.uamishop.ventas.repository.CarritoRepository;
 
 import jakarta.transaction.Transactional;
 
+@Service
 public class CarritoService {
 
   @Autowired
-  CarritoRepository carritoRepository;
+  private CarritoRepository carritoRepository;
 
+  @Transactional
   public Carrito crear(ClienteId clienteId) {
-    Carrito carrito = Carrito.crear(clienteId);
-    return carrito;
+    // Carrito carrito = Carrito.crear(clienteId);
+    // return carritoRepository.save(carrito);
+
+    Optional<Carrito> carritoExistente = carritoRepository
+        .findByClienteIdAndEstado(clienteId, EstadoCarrito.ACTIVO);
+
+    // Si existe retornarlo
+    if (carritoExistente.isPresent()) {
+      return carritoExistente.get();
+      // si no, crear uno nuevo
+    } else {
+      Carrito nuevoCarrito = Carrito.crear(clienteId);
+      return carritoRepository.save(nuevoCarrito);
+    }
   }
 
+  @Transactional
   public Carrito obtenerCarrito(CarritoId carritoId) {
 
     Optional<Carrito> carrito = carritoRepository.findById(carritoId);
@@ -30,11 +50,35 @@ public class CarritoService {
   }
 
   @Transactional
+  public Carrito agregarProducto(CarritoId carritoId, ProductoRef productoRef, int cantidad,
+      Money precioUnitario) {
+    Carrito carrito = obtenerCarrito(carritoId);
+    carrito.agregarProducto(productoRef, cantidad, precioUnitario);
+
+    return carritoRepository.save(carrito);
+  }
+
+  @Transactional
+  public Carrito modificarCantidad(CarritoId carritoId, UUID productoId, int nuevaCantidad) {
+    Carrito carrito = obtenerCarrito(carritoId);
+    carrito.modificarCantidad(productoId, nuevaCantidad);
+
+    return carritoRepository.save(carrito);
+  }
+
+  @Transactional
+  public Carrito eliminarProducto(CarritoId carritoId, UUID productoId) {
+    Carrito carrito = obtenerCarrito(carritoId);
+    carrito.eliminarProducto(productoId);
+
+    return carritoRepository.save(carrito);
+  }
+
+  @Transactional
   public Carrito vaciar(CarritoId carritoId) {
 
     // Validamos que el carrito exista
-    Carrito carrito = carritoRepository.findById(carritoId)
-        .orElseThrow(() -> new IllegalArgumentException("Carrito inexistente"));
+    Carrito carrito = obtenerCarrito(carritoId);
 
     // Vaciamos el carrito
     carrito.vaciar();
@@ -47,8 +91,7 @@ public class CarritoService {
   public Carrito iniciarCheckout(CarritoId carritoId) {
 
     // Validamos que el carrito exista
-    Carrito carrito = carritoRepository.findById(carritoId)
-        .orElseThrow(() -> new IllegalArgumentException("Carrito inexistente"));
+    Carrito carrito = obtenerCarrito(carritoId);
 
     // Iniciamos checkout
     carrito.iniciarCheckout();
@@ -61,8 +104,7 @@ public class CarritoService {
   public Carrito completarCheckout(CarritoId carritoId) {
 
     // Validamos que el carrito exista
-    Carrito carrito = carritoRepository.findById(carritoId)
-        .orElseThrow(() -> new IllegalArgumentException("Carrito inexistente"));
+    Carrito carrito = obtenerCarrito(carritoId);
 
     // Completamos Checkout
     carrito.completarCheckout();
@@ -75,8 +117,7 @@ public class CarritoService {
   public Carrito abandonar(CarritoId carritoId) {
 
     // Validamos que el carrito exista
-    Carrito carrito = carritoRepository.findById(carritoId)
-        .orElseThrow(() -> new IllegalArgumentException("Carrito inexistente"));
+    Carrito carrito = obtenerCarrito(carritoId);
 
     // Abandonamos
     carrito.abandonar();
