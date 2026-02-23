25054f09ee5807311c39dcf93001423628c9096c
Hans Diaz <hansdacc@gmail.com>
2026-02-22 23:33:08 -0600

    chore: cambio de directorio para test de ordenes y guia para la creacion de recursos

---
+++ flujo-creacion.md
@@ -0,0 +1,4 @@
+# Flujo de creación de recursos
+
+1. Debe existir una categoría para crear un producto, si no existe, crear una.
+2. Para crear una orden, debe existir un producto, si no existe, crear uno.
--- src/test/java/com/chefsitos/uamishop/ordenes/controller/OrdenControllerIntegrationTest.java
+++ src/test/java/com/chefsitos/uamishop/ordenes/controller/OrdenControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.chefsitos.uamishop.ordenes.integration;
+package com.chefsitos.uamishop.ordenes.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
