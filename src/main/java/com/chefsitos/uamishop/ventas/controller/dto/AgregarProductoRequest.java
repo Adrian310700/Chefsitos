ebbcb6d78df5f5133ddd5dae6dc24bed047ca75e
terPM <teresaguadalupepimentel@gmail.com>
2026-03-08 20:18:32 -0600

    Modificacion de DTO para agregar un producto al carrito (aceptaba 0, ya no)

--- src/main/java/com/chefsitos/uamishop/ventas/controller/dto/AgregarProductoRequest.java
+++ src/main/java/com/chefsitos/uamishop/ventas/controller/dto/AgregarProductoRequest.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import jakarta.validation.constraints.*;
 
 public record AgregarProductoRequest(
-    @NotNull(message = "El ID del producto es obligatorio") UUID productoId,
+        @NotNull(message = "El ID del producto es obligatorio") UUID productoId,
 
-    @NotNull(message = "La cantidad del producto es obligatoria") @Max(value = 10, message = "La cantidad del producto no puede exceder 10") @Min(value = 0, message = "La cantidad del producto no puede ser negativa") int cantidad) {
+        @NotNull(message = "La cantidad del producto es obligatoria") @Max(value = 10, message = "La cantidad del producto no puede exceder 10") @Min(value = 1, message = "La cantidad debe ser al menos 1") int cantidad) {
 }
