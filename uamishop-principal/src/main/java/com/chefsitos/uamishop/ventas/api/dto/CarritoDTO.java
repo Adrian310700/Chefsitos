32c06721c2bc7f095ed0334cf0687a3276fcbc3e
terPM <teresaguadalupepimentel@gmail.com>
2026-03-22 19:05:44 -0600

    Cambio de carpeta de DTOs para en controller en el microservicio de ventas

--- uamishop-principal/src/main/java/com/chefsitos/uamishop/ventas/api/dto/CarritoDTO.java
+++ uamishop-principal/src/main/java/com/chefsitos/uamishop/ventas/api/dto/CarritoDTO.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-import com.chefsitos.uamishop.ventas.domain.aggregate.Carrito;
-
 public record CarritoDTO(
     UUID carritoId,
     UUID clienteId,
@@ -15,16 +13,4 @@ public record CarritoDTO(
     BigDecimal subtotal,
     BigDecimal total,
     String moneda) {
-
-  public static CarritoDTO from(Carrito carrito) {
-    return new CarritoDTO(
-        carrito.getCarritoId().getValue(),
-        carrito.getClienteId().valor(),
-        carrito.getItems().stream().map(ItemCarritoDTO::from).toList(),
-        carrito.getDescuentos().stream().map(DescuentoDTO::from).toList(),
-        carrito.getEstado().name(),
-        carrito.calcularSubtotal().cantidad(),
-        carrito.calcularTotal().cantidad(),
-        carrito.calcularTotal().moneda());
-  }
 }
